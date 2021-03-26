0be8c7b2478185aedc47fef6c2eab84c0ac50814
Prashant <prasbas97@gmail.com>
2021-03-26 15:28:04 +0545

    Added repository class and commented

--- app/src/main/java/com/example/todoapp/data/AppDatabase.java
+++ app/src/main/java/com/example/todoapp/data/AppDatabase.java
@@ -1,41 +1,44 @@
 package com.example.todoapp.data;
 
 import android.content.Context;
-
 import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
-
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+
 @Database(entities = {Task.class}, version = 1, exportSchema = false)
 @TypeConverters(DateConverter.class)
 
-
+// TODO (01) Create an abstract class that extends RoomDatabase.
 public abstract class AppDatabase extends RoomDatabase {
 
+
+    // TODO (04) Declare a @Volatile sINSTANCE variable.
 public  static AppDatabase INSTANCE;
 
-
+    // TODO (02) Declare an abstract value of type TaskDao.
 public abstract TodoDao todoDao();
 
 
 public static final int NUMBER_OF_THREADS=4;
 
-static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
 
 
-
+   // TODO (06) Inside the synchronized block:
     public  static AppDatabase getDatabase(Context context){
         if (INSTANCE ==null){
             synchronized (AppDatabase.class){
                 if (INSTANCE==null){
+
                     INSTANCE= Room.databaseBuilder(context.getApplicationContext(),AppDatabase.class,
                             "todo.db").build();
                 }
